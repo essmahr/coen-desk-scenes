f17e69ba11da8c8a7616dc8b94499c9c70c92615
Scott Mahr <essmahr@gmail.com>
2018-12-03 20:29:18 -0800

    make scene building script fancier

--- build-scenes.js
+++ build-scenes.js
@@ -2,20 +2,57 @@ const path = require('path');
 const fs = require('fs');
 
 const IMAGE_EXT = '.jpeg';
+const DEFAULT_FIELD_KEYS = ['quote'];
+
+const defaultFields = DEFAULT_FIELD_KEYS.reduce((o, k) => {
+  o[k] = '';
+  return o;
+}, {});
+
 const PWD = process.env.PWD;
 
 const imagesDir = path.join(PWD, 'src', 'images');
-const scenesJson = path.join(PWD, 'src', 'data', 'scenes.json');
+const scenesPath = path.join(PWD, 'src', 'data', 'scenes.json');
+
+const scenesJson = JSON.parse(fs.readFileSync(scenesPath));
 
 const imageJson = fs
   .readdirSync(imagesDir)
   .filter(file => path.extname(file) === IMAGE_EXT)
   .map(file => {
-    const [film, timestamp] = file.slice(0, -1 * IMAGE_EXT.length).split('_');
+    const [film, timestampStr] = file
+      .slice(0, -1 * IMAGE_EXT.length)
+      .split('_');
+    const timestamp = timestampStr.split('.').join(':');
+
+    const found = scenesJson.find(
+      ({ film: searchFilm, timestamp: searchTimeStamp }) => {
+        return timestamp === searchTimeStamp && searchFilm === film;
+      }
+    );
+
+    if (found) {
+      DEFAULT_FIELD_KEYS.forEach(field => {
+        if (!found[field] || found[field] === '') {
+          console.log(`${film} / ${timestamp} is missing ${field}!`);
+        }
+      });
+
+      return {
+        ...defaultFields,
+        ...found,
+        film,
+        timestamp,
+      };
+    }
+
+    console.log(`adding new scene: ${film} / ${timestamp}`);
+
     return {
+      ...defaultFields,
       film,
-      timestamp: timestamp.split('.').join(':'),
+      timestamp,
     };
   });
 
-fs.writeFileSync(scenesJson, JSON.stringify(imageJson, null, 2));
+fs.writeFileSync(scenesPath, JSON.stringify(imageJson, null, 2));
