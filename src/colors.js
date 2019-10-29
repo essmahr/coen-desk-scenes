fb29d629669e9399c959b2cde2d212bb8e41fd92
Scott Mahr <essmahr@gmail.com>
2019-10-28 21:33:55 -0700

    remove unused files

--- src/colors.js
+++
@@ -1,9 +0,0 @@
-export const serif = 'IBM Plex Serif';
-
-export const background = '#080605';
-
-export const textPrimary = '#dbdbd3';
-
-export const textSecondary = '#AFAEA8';
-
-export const border = '#4B4A46';
